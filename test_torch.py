59680ef4a4e5c61ec9645965da1b6fac90be6607
Nolan Hedglin <nhedglin@mac.mynetworksettings.com>
2025-08-04 09:45:40 -0400

    Ignore Test Figure folder, __pycache__, and test_torch.py

--- test_torch.py
+++
@@ -1,6 +0,0 @@
-from ViT_example import *
-
-model = VisionTransformer(img_size=224, patch_size=16, num_classes=10)
-dummy_img = torch.randn(1, 3, 224, 224)  # Batch of 1, 3 channels, 224x224
-logits = model(dummy_img)
-print(logits.shape)  # Should be [1, 10]
